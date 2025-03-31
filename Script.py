09de07fab9cca5cb4e63247215347eacb87e4ba1
Julianne <julianne.cocq@gmail.com>
2025-03-31 14:47:59 +0200

    dico kmers

--- Script.py
+++ Script.py
@@ -1,5 +1,6 @@
 import gzip
 from Bio import SeqIO    
+from collections import defaultdict
     
 def read_gz(filename):
         """
@@ -21,7 +22,37 @@ def read_gz(filename):
             else:  
                 raise ValueError(f"file format not supported: '{filename}'")
             
+def kmers(sequence, k)-> dict[str:tuple[int:int]]:
+        '''
+        Collects all possible on the sequence passed in parameter associated with their occurrences.
 
+        Parameters:
+        :param query: a nucleotide sequence corresponding to the query in which we are looking for every Kmers possible
+
+        Returns:
+        :return dict[str:tuple[int:int]]: A dictionary with Kmers sequences as keys and a list of occurrences positions
+        on the query as values.
+
+        Examples:
+        >>> a = Kmers(3)
+        >>> a.query_kmers('ATCGAAATCG')
+        {'ATC': [(0, 2), (6, 8)], 'TCG': [(1, 3), (7, 9)], 'CGA': [(2, 4)], 'GAA': [(3, 5)], 'AAA': [(4, 6)], 'AAT': [(5, 7)]}
+        
+        # Cas extrême : séquence trop courte
+        >>> a.query_kmers('A')  
+        {}
+        '''
+        #Initializes a default dictionary with list as values
+        kmers = defaultdict(list)
+
+        #Loop through every position that can contain a kmer on the query sequence
+        for pos in range(len(sequence)-k+1):
+            #Extract the kmers and add it as a key to the dictionary and then add their occurrences positions as values 
+            kmers[sequence[pos:pos+k]].append((pos,pos+k-1))
+
+        #Transform the default dictionary into a Python dictionary
+        return dict(kmers)
+    
 if __name__ == '__main__':
     k = 'CCC'
     for s in read_gz("Level0.fa.gz"):
