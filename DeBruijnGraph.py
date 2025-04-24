e91e594f415ba3211599790d55a602c5881e8f40
maxime soltane <maxime.soltane.etu@univ-lille.fr>
2025-04-24 03:24:48 +0200

    DeBruijnGraph docstrings

--- DeBruijnGraph.py
+++ DeBruijnGraph.py
@@ -1,26 +1,64 @@
 from collections import defaultdict
-
+from typing import Dict, List
 
 class DeBruijnGraph :
+    """
+    A class that represents a de Bruijn graph built with a collection of kmers.
+    The graph is used to assembly genomes by overlapping kmers. 
+    """
 
-    def __init__(self, kmers_dict):
+    def __init__(self, kmers_dict: Dict[str, int]) -> None:
         self.__kmers_dict = kmers_dict
         self.graph = defaultdict(list)
         self.__build_connections()
 
-    def __build_connections (self):
+    def __build_connections (self) -> None:
+        """
+        Builds graph connections from the kmers, each kmer is split into a prefixe and a suffix.
+        """
         for kmer in self.__kmers_dict:
             prefix = kmer[:-1]
             suffix = kmer[1:]
             self.graph[prefix].append(suffix)
 
-    def get_graph(self):
+    def get_graph(self) -> Dict[str, List[str]]:
+        """
+        Gets the graph representation.
+        """
         return self.graph
     
-    def get_successors(self, node):
+    def get_successors(self, node: str) -> List[str]:
+        """
+        Gets the list of successors for a node.
+
+        Parameters:
+        node: A node in the graph
+
+        Returns:
+        A list of successor nodes
+
+        Example:
+        >>> graph = DeBruijnGraph({'ATG':1})
+        >>> graph.get_successors('AT')
+        ['TG']
+        """
         return self.graph.get(node, [])
 
-    def get_predecessors(self, node):
+    def get_predecessors(self, node: str) -> List[str]:
+        """
+        Gets the list of predecessors for a node.
+
+        Parameters:
+        node: A node in the graph
+
+        Returns:
+        A list of predecessors nodes
+
+        Example:
+        >>> graph = DeBruijnGraph({'ATG':1})
+        >>> graph.get_predecessors('TG')
+        ['AT']
+        """
         preds = []
         for prefix in self.graph:
             for suffix in self.graph[prefix]:
@@ -28,7 +66,24 @@ class DeBruijnGraph :
                     preds.append(prefix)
         return preds
 
-    def simple_path(self):
+    def simple_path(self) -> List[str]:
+        """
+        Tries to find a simple path starting from a node with no predecessors.
+        Only follows paths with a single successor.
+
+        Returns:
+        A list of nodes forming the path
+
+        Examples:
+        >>> graph = DeBruijnGraph({'ATG':1, 'TGG':1, 'GGT':1})
+        >>> graph.simple_path()
+        ['AT', 'TG', 'GG', 'GT']
+
+        # No clear start node
+        >>> graph = DeBruijnGraph({'AAA': 1, 'AAT': 1, 'ATA': 1, 'TAA': 1})
+        >>> graph.simple_path()
+        []
+        """
         visited = set()
         for start in self.graph:
             if len(self.get_predecessors(start)) == 0:
@@ -44,7 +99,18 @@ class DeBruijnGraph :
                 return path
         return []
 
-    def assemble_sequence(self):
+    def assemble_sequence(self) -> str:
+        """
+        Assembles a sequence from the simple path.
+
+        Returns:
+        An assembled nucleotidic sequence
+
+        Example:
+        >>> graph = DeBruijnGraph({'ATG': 1, 'TGG': 1, 'GGT': 1})
+        >>> graph.assemble_sequence()
+        'ATGGT'
+        """
         path = self.simple_path()
 
         if not path:
