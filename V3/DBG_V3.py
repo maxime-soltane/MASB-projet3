1bb3e7c20c85f4aa2bb5a421e0c8f495c78fc77b
Julianne <julianne.cocq@gmail.com>
2025-05-03 11:15:05 +0200

    Remove tips

---
+++ V3/DBG_V3.py
@@ -0,0 +1,404 @@
+from collections import defaultdict
+from typing import List, Set, Tuple
+
+
+class DBG:
+
+    def __init__(self, kmers_dict):
+        self.__kmers_dict = kmers_dict
+        self.__graph = defaultdict(list)
+        self.__build_graph()
+    
+    def __build_graph(self):
+        self.__graph = defaultdict(list)  # Réinitialise le graphe
+    
+        for kmer in self.__kmers_dict:
+            if len(kmer) < 2:
+                continue
+            prefix = kmer[:-1]
+            suffix = kmer[1:]
+            self.__graph[prefix].append(suffix)
+
+    #Get method
+
+    def get_successors(self, node: str) -> List[str]:
+        """
+        Returns the list of successors for a given node.
+
+        Parameter:
+        node: A node in the graph
+
+        Returns:
+        A list of successors nodes
+        
+        Examples:
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_successors("AT")
+        ['TG']
+        >>> g.get_successors("TG")
+        ['GG', 'GT']
+        """
+        return self.__graph.get(node, [])
+    
+    def get_predecessors (self, node: str) -> List[str]:
+        """
+        Returns the list of predecessors for a given node.
+
+        Parameter:
+        node: A node in the graph
+
+        Returns:
+        A list of predecessors nodes
+        
+        Examples:
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_predecessors("GG")
+        ['TG']
+        >>> g.get_predecessors("AT")
+        []
+        """
+        predecessors = []
+        for prefix, suffixes in self.__graph.items():
+            if node in suffixes:
+                predecessors.append(prefix)
+        return predecessors
+
+    def get_graph(self):
+        """
+        Returns the forward de Bruijn graph as a dictionary.
+
+        Returns:
+        A dictionary where keys are prefixes and values are lists of suffixes
+
+        Example:
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG', 'GT'], 'GG': ['GA']}
+        """
+        return dict(self.__graph)
+    
+    def get_kmers_dict(self):
+        return self.__kmers_dict
+    
+    # Construction de chemin
+
+    def __extend_forward(self, start_node):
+        """
+        Étend le chemin vers l'avant à partir de `start_node` tant que les nœuds ont un seul successeur.
+        
+        Paramètre:
+        start_node: Nœud de départ.
+        
+        Retourne:
+        Liste des nœuds parcourus (y compris `start_node`).
+
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG', 'GT'], 'GG': ['GA']}
+        >>> g._DBG__extend_forward('AT')
+        ['AT', 'TG']
+        >>> g._DBG__extend_forward('TG')
+        ['TG']
+        >>> g._DBG__extend_forward('GG')
+        ['GG', 'GA']
+        >>> g._DBG__extend_forward('GT')
+        ['GT']
+        >>> g._DBG__extend_forward('GG')
+        ['GG', 'GA']
+        >>> g._DBG__extend_forward('GA')
+        ['GA']
+        """
+        path = [start_node]
+        current_node = start_node
+
+        while True:
+            successors = self.get_successors(current_node)
+            if len(successors) != 1:
+                break  # Bifurcation ou fin du chemin
+
+            next_node = successors[0]
+            predecessors_of_next = self.get_predecessors(next_node)
+
+            if len(predecessors_of_next) != 1 or predecessors_of_next[0] != current_node:
+                break  # Successeur a plusieurs prédécesseurs ou lien non-unique
+
+            path.append(next_node)
+            current_node = next_node
+
+        return path
+
+    def __extend_backward(self, start_node):
+        """
+        Étend le chemin vers l'arrière à partir de `start_node` tant que les nœuds ont un seul prédécesseur.
+        
+        Paramètre:
+        start_node: Nœud de départ.
+        
+        Retourne:
+        Liste des nœuds parcourus (sans `start_node`, pour éviter les duplications).
+
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG', 'GT'], 'GG': ['GA']}
+        >>> g._DBG__extend_backward('AT')
+        []
+        >>> g._DBG__extend_backward('TG')
+        ['AT']
+        >>> g._DBG__extend_backward('GG')
+        []
+        >>> g._DBG__extend_backward('GT')
+        []
+        >>> g._DBG__extend_backward('GG')
+        []
+        >>> g._DBG__extend_backward('GA')
+        ['GG']
+        """
+        path = []
+        current_node = start_node
+
+        while True:
+            predecessors = self.get_predecessors(current_node)
+            if len(predecessors) != 1:
+                break  # Bifurcation ou début du chemin
+
+            pred = predecessors[0]
+            successors_of_pred = self.get_successors(pred)
+
+            if len(successors_of_pred) != 1 or successors_of_pred[0] != current_node:
+                break  # Prédécesseur a plusieurs successeurs ou lien non-unique
+
+            path.insert(0, pred)  # On ajoute au début pour garder l'ordre
+            current_node = pred
+
+        return path
+
+    def __simple_path(self, start_node):
+        """
+        Combine l'extension vers l'arrière et l'avant pour former un chemin linéaire maximal.
+        
+        Paramètre:
+        start_node: Nœud de départ.
+        
+        Retourne:
+        Chelin linéaire complet (arrière + avant).
+
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG', 'GT'], 'GG': ['GA']}
+        >>> g._DBG__simple_path('AT')
+        ['AT', 'TG']
+        >>> g._DBG__simple_path('TG')
+        ['AT', 'TG']
+        >>> g._DBG__simple_path('GG')
+        ['GG', 'GA']
+        >>> g._DBG__simple_path('GT')
+        ['GT']
+        >>> g._DBG__simple_path('GA')
+        ['GG', 'GA']
+        """
+        backward_path = self.__extend_backward(start_node)
+        forward_path = self.__extend_forward(start_node)
+        full_path = backward_path + forward_path  # Fusion sans duplication de `start_node`
+
+        return full_path
+
+
+    # Options d'assemblage
+
+    def is_tip(self, path: List[str], threshold: int=5) -> bool:
+        """
+        Checks if a given path is a tip (a short dead-end path).
+
+        Parameters:
+        path: The list of nodes representing a path
+        threshold: The maximum length to be considered a tip
+
+        Returns:
+        True if it's a tip, False otherwise
+
+        Examples:
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGC':1})
+        >>> p = g._DBG__simple_path('AT')
+        >>> print(p)
+        ['AT', 'TG', 'GG', 'GC']
+        >>> g.is_tip(p)
+        True
+        >>> g.is_tip(["AT", "TG", "GG", "GC"], 2)
+        False
+        """
+        if not path or len(path) >= threshold:
+            return False
+        
+        return len(self.get_successors(path[-1])) == 0
+
+    def find_all_tips(self, threshold:int=5) -> List[Tuple[List[str], Set[str]]]:
+        """
+        Detects all tips in the graph under a certain threshold
+
+        Parameter:
+        threshold: The maximum path length considered as a tip
+
+        Returns:
+        A list of tuples containing the path and the kmers used
+
+        Examples:
+        #Cas 1 : 1 seul tip
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGC':1})
+        >>> g._DBG__simple_path('AT')
+        ['AT', 'TG', 'GG', 'GC']
+        >>> g.find_all_tips()
+        [['AT', 'TG', 'GG', 'GC']]
+
+        #Cas 2 : 3 tips
+        >>> g = DBG({'ATG': 1, 'TGG': 1, 'GGA': 1, 'GGT': 1, 'GTC': 1, 'TCC': 1, 'TCA': 1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG'], 'GG': ['GA', 'GT'], 'GT': ['TC'], 'TC': ['CC', 'CA']}
+        >>> g.find_all_tips(2)
+        [['GG', 'GA'], ['TC', 'CC'], ['TC', 'CA']]
+
+        >>> # Cas 3: Aucun tip (tous les chemins ont des successeurs)
+        >>> g = DBG({'ATG': 1, 'TGG': 1, 'GGG': 1, 'GGT': 1, 'GTT': 1, 'TTT': 1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG'], 'GG': ['GG', 'GT'], 'GT': ['TT'], 'TT': ['TT']}
+        >>> len(g.find_all_tips())
+        0
+
+        # Cas 4: Tip plus long que le seuil (ne doit pas être détecté)
+        >>> g = DBG({'ATG':1, 'TGC':1, 'GCA':1, 'CAA':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GC'], 'GC': ['CA'], 'CA': ['AA']}
+        >>> len(g.find_all_tips(3))  
+        0
+        """
+        tips = []
+        visited = set()
+
+        for node in self.__graph:
+            if node in visited:
+                continue
+
+            path = self.__simple_path(node)
+            if self.is_tip(path, threshold):
+                tips.append(path)
+                visited.update(path)
+            
+            successors = self.get_successors(node)
+            if len(successors) > 1:
+                for succ in successors:
+                    if succ in visited:
+                        continue
+                    path = self.__simple_path(succ)
+                    if self.is_tip(path, threshold):
+                        full_path = [node] + path
+                        tips.append(full_path)
+                        visited.update(path)
+
+        return tips
+
+    
+    def remove_tips(self, threshold=5):
+        """
+        Remove tips from the graph based on a length threshold.
+ 
+        Parameter:
+        threshold: The maximum length to be considered a tip
+ 
+        Parameter:
+        threshold: Maximum allowed tip length
+ 
+        Examples:
+        # Cas de base : une branche principale avec un court tip
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1})
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG', 'GT'], 'GG': ['GA']}
+        >>> g.remove_tips(2)
+        >>> g.get_graph()
+        {'AT': ['TG'], 'TG': ['GG'], 'GG': ['GA']}
+        >>> g.get_kmers_dict()
+        {'ATG': 1, 'TGG': 1, 'GGA': 1}
+        """
+        k = len(next(iter(self.__kmers_dict))) 
+        tips = self.find_all_tips(threshold)
+        
+        for tip in tips:  
+                
+            # Reconstruit la séquence et extrait les k-mers
+            sequence = tip[0] + ''.join(node[-1] for node in tip[1:])
+            for i in range(len(sequence) - k + 1):
+                kmer = sequence[i:i+k]
+                self.__kmers_dict.pop(kmer, None)
+        
+        self.__build_graph()
+
+
+
+    # Assemblage de séquence
+
+    def __assemble_sequence(self, path: List[str]) -> str:
+        """
+        Assembles a sequence from a nodes-made path.
+        
+        Parameter:
+        path: A list of nodes representing the contig path
+        
+        returns:
+        A single string representing the assembled sequence
+
+        Examples:
+        #Cas de base
+        >>> g = DBG({'ATG':1, 'TGG':1, 'GGC':1})        
+        >>> g._DBG__assemble_sequence(['AT', 'TG', 'GG', 'GC'])
+        'ATGGC'
+
+        #Cas 2 : chemin vide
+        >>> g = DBG({})        
+        >>> g._DBG__assemble_sequence([])
+        ''
+        """
+        if not path:
+            return ""
+        return path[0] + ''.join(node[-1] for node in path[1:])
+
+    def get_all_contigs(self, output_file: str = "output_file.fa", tip_threshold = 3, bubble_threshold = 50) -> None:
+        """
+        Extracts all the contigs from the graph, writes them in a fasta file and deletes the used kmers.
+
+        Parameters:
+        output_file: Path to the output fasta file
+        tip_threshold: The threshold for tip removal
+        bubble_threshold: The threshold for bubble removal   
+        """
+        self.remove_tips(tip_threshold)
+        self.remove_bubbles(bubble_threshold)
+
+        contig_num = 1
+        
+        with open(output_file, 'w') as out:
+            for start_kmer in list(self.kmers_dict.keys()):
+                if start_kmer not in self.kmers_dict:
+                    # Kmers already processed
+                    continue  
+
+                start_node = start_kmer[:-1]
+                path, used_kmers = self.__simple_path(start_node)
+
+                if not path:
+                    continue
+
+                for k in used_kmers:
+                    self.kmers_dict.pop(k, None)
+
+                contig = self.__assemble_sequence(path)
+                out.write(f">contig_{contig_num}_len_{len(contig)}\n")
+                out.write('\n'.join(contig[i:i+60] for i in range(0, len(contig), 60)) + '\n')
+                contig_num += 1
+
+        print(f"Contigs générés : {contig_num - 1}")
+        print(f"{output_file} was generated\n")
+
+if __name__ == '__main__':
+    g = DBG({'ATG':1, 'TGG':1, 'GGA':1, 'TGT':1, 'GAC': 1, 'GTC': 1})
+    print(g.find_all_tips(3))
+    print(g.get_graph())
+    g.remove_tips(2)
+    print(g.get_graph())
---
+++ V3/Script.py
@@ -0,0 +1,126 @@
+#Import des modules nécessaires au fonctionnement du script
+import gzip
+from Bio import SeqIO   
+from collections import defaultdict
+from typing import Dict
+from matplotlib import pyplot as plt
+
+def read_gz(filename: str):
+        """
+        Open a Fasta or FastQ file, compressed (.gz) or not, in reading mode and returns sequence's iterator.
+
+        Parameter:
+        filename: the name of the file to work on
+
+        Raises:
+        ValueError: if file format isn't supported
+        """
+        #Define the correct open function wether the file is compressed or not
+        open_func = gzip.open if filename.endswith(".gz") else open  
+
+        with open_func(filename, "rt") as file:
+            # Open FastQ file
+            if filename.endswith(('.fastq', '.fq', '.fastq.gz', '.fq.gz')):  
+                yield from SeqIO.parse(file, 'fastq')
+            # Open FastA file    
+            elif filename.endswith(('.fasta', '.fna', '.fa', '.fasta.gz', '.fna.gz', '.fa.gz')):
+                yield from SeqIO.parse(file, 'fasta')
+            else:  
+                raise ValueError(f"file format not supported: '{filename}'")
+            
+def kmers (sequence: str, k: int) -> Dict[str, int]:
+    """
+    Generates all kmers from a sequence and count their occurrences.
+
+    Parameters:
+    sequence: A nucleotidic sequence
+    k: The size of the kmer
+
+    Returns:
+    A dictionary of kmers and their counts
+
+    Examples:
+    >>> kmers("ATCGGCAT", 3)
+    {'ATC': 1, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1}
+    >>> kmers("AAAAAA", 2)
+    {'AA': 5}
+    >>> kmers("ATG", 5)
+    {}
+    """
+    kmers = defaultdict(int)
+    # Browse every positions where kmers are possibles
+    for pos in range(len(sequence)-k+1):
+        # Add them to the dictionary with their number of occurrences
+        kmers[sequence[pos:pos+k]] += 1
+    return kmers
+
+def check_kmers(kmer: str,dict_kmers: Dict[str, int]) -> bool:
+     """
+    >>> check_kmers("ATC", {'ATC': 1, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1})
+    True
+     """
+     return kmer in dict_kmers
+
+def add_kmers(kmer, dict_kmers):
+    """
+    >>> add_kmers("ATT", {'ATC': 1, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1})
+    {'ATC': 1, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1, 'ATT': 1}
+    >>> add_kmers("ATC", {'ATC': 1, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1})
+    {'ATC': 2, 'TCG': 1, 'CGG': 1, 'GGC': 1, 'GCA': 1, 'CAT': 1}
+    """
+    if kmer not in dict_kmers:
+        dict_kmers[kmer] = 1
+    else:
+        dict_kmers[kmer] += 1
+
+    return dict_kmers
+
+def kmers_filter (kmers_dict: Dict[str, int], threshold: int= 1) -> Dict[str, int]:
+    """
+    Filters kmers according to a minimum count threshold.
+
+    Parameters:
+    kmers_dict: The dictionary of kmer counts
+    threshold: The minimum count to keep a kmer
+
+    Returns:
+    A filtered dictionary of kmers
+
+    Examples:
+    >>> k_dict = {"A" : 4, "B" : 1, "C" : 12, "D" : 5}
+    >>> kmers_filter(k_dict, 5)
+    {'C': 12, 'D': 5}
+    >>> kmers_filter({'A': 2, 'T': 3}, 3)
+    {'T': 3}
+    """
+    f_kmers = defaultdict(int)
+    for kmer, count in kmers_dict.items():
+        if count >= threshold:
+            f_kmers[kmer] = count
+    
+    return f_kmers
+
+def abundance_hist(kmers_dict: Dict[str, int]) -> None:
+    """
+    Plots a histogram of kmer abundance on a logarithmic y axis.
+
+    Parameter:
+    kmer_dict: The dictionary of kmers and their counts
+    """
+    abundance_hist = defaultdict(int)
+    for count in kmers_dict.values():
+            abundance_hist[count] += 1
+
+    x = sorted(abundance_hist.keys())
+    y = [abundance_hist[i] for i in x]
+
+    plt.figure(figsize=(12, 6))
+    plt.bar(x, y, color="dodgerblue", width=1.0, edgecolor='black')
+
+    plt.title("K-mer abundance histogram")
+    plt.xlabel("K-mer multiplicity")
+    plt.yscale('log')
+    plt.ylabel("Number of Distinct K-mers (log scale)")
+
+    plt.tight_layout()
+    plt.show()
